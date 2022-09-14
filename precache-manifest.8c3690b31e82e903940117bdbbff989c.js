1b04169db87c9ad34e80e29bb1249131f2c777c1
Shivani  Sharma <shivanibbc22@gmail.com>
2022-09-14 16:15:43 +0530

    Updates

---
+++ precache-manifest.8c3690b31e82e903940117bdbbff989c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91c1febf2a23fce6271b24077a959d1e",
+    "url": "/E-Shopping/index.html"
+  },
+  {
+    "revision": "75a3280240f0f4fd2457",
+    "url": "/E-Shopping/static/css/main.634e35a9.chunk.css"
+  },
+  {
+    "revision": "32b650c25670705f154e",
+    "url": "/E-Shopping/static/js/2.7124a6e1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/E-Shopping/static/js/2.7124a6e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75a3280240f0f4fd2457",
+    "url": "/E-Shopping/static/js/main.c582170d.chunk.js"
+  },
+  {
+    "revision": "459eb7442959dc6f1d30",
+    "url": "/E-Shopping/static/js/runtime-main.c2e421f2.js"
+  },
+  {
+    "revision": "028978f40ca009a385c6fb19486bdc53",
+    "url": "/E-Shopping/static/media/book.028978f4.jpg"
+  },
+  {
+    "revision": "67a487675e594f3b274f76d190aa011f",
+    "url": "/E-Shopping/static/media/smartphone.67a48767.jpg"
+  },
+  {
+    "revision": "4a0dbe85527b88bfcea004afbf6c9fb8",
+    "url": "/E-Shopping/static/media/speaker.4a0dbe85.jpg"
+  }
+]);
